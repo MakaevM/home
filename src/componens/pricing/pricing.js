98913501348ce4ed5c1df03cc8fc835bdc3f5c6c
Maksat <makaevmaksat2001@gmail.com>
2022-11-13 15:30:17 +0600

    initial

---
+++ src/componens/pricing/pricing.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Pricing = () => {
+    return (
+        <div id="pricing">
+            <div className="container">
+                <div className="pricing--general">
+                    <p>PRICING</p>
+                    <h1>CHOOSE YOUR PACKAGE
+                        <br />FOR WORKOUT</h1>
+                </div>
+
+            </div>
+
+        </div>
+    );
+};
+
+export default Pricing;
